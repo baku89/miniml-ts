02ffeb06f331d4248eb6716af333a34373426e5d
Baku Hashimoto <mail@baku89.com>
2021-10-21 17:13:23 +0900

    Implement ast printer

--- src/exp/index.ts
+++ src/exp/index.ts
@@ -2,26 +2,35 @@ export type Exp = Var | Int | Bool | BinOp | If
 
 interface IExp {
 	type: string
-}
 
-export class Program implements IExp {
-	public type: 'program' = 'program'
-	public constructor(public exp: Exp) {}
+	print(): string
 }
 
 export class Var implements IExp {
 	public type: 'var' = 'var'
 	public constructor(public id: string) {}
+
+	public print() {
+		return this.id
+	}
 }
 
 export class Int implements IExp {
 	public type: 'int' = 'int'
 	public constructor(public value: number) {}
+
+	public print() {
+		return this.value.toString()
+	}
 }
 
 export class Bool implements IExp {
 	public type: 'bool' = 'bool'
 	public constructor(public value: boolean) {}
+
+	public print() {
+		return this.value.toString()
+	}
 }
 
 export class BinOp implements IExp {
@@ -31,6 +40,10 @@ export class BinOp implements IExp {
 		public op: '+' | '*' | '<',
 		public right: Exp
 	) {}
+
+	public print(): string {
+		return `(${this.left.print()} ${this.op} ${this.right.print()})`
+	}
 }
 
 export class If implements IExp {
@@ -40,4 +53,11 @@ export class If implements IExp {
 		public consequent: Exp,
 		public alternate: Exp
 	) {}
+
+	public print(): string {
+		const test = this.test.print()
+		const consequent = this.consequent.print()
+		const alternate = this.alternate.print()
+		return `if ${test} then ${consequent} else ${alternate}`
+	}
 }
