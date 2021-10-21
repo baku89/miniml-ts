0ddbed6db5d84f18da40f291338cd21bea847c65
Baku Hashimoto <mail@baku89.com>
2021-10-21 14:41:31 +0900

    Make the pegjs parser work

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -6,28 +6,34 @@ import * as exp from '../exp'
 const ParserDefinition = `
 Program = Exp
 
-Exp = Int / Bool
+Exp = BinOp / Int / Bool
 
 Int = str:$([0-9]+)
 	{
-		const value = parseInt(str)
-		return {
-			type: 'int',
-			value
-		}
+		const val = parseInt(str)
+		return new exp.Int(val)
 	}
 
 Bool = str:$("true" / "false")
 	{
-		const value = str === 'true'
-		return {
-			tyoe: 'bool',
-			value
-		}
+		const val = str === 'true'
+		return new exp.Bool(val)
+	}
+
+Op = "+" / "*" / "<"
+
+BinOp = left:(Int / Bool) op:Op right:(Int / Bool)
+	{
+		return new exp.BinOp(left, op, right)
 	}
 `
 
-const parser = peggy.generate(ParserDefinition)
+const parserSource = peggy.generate(ParserDefinition, {
+	exportVar: {exp},
+	output: 'source',
+})
+
+const parser = eval(parserSource)
 
 export function parse(str: string): exp.Exp {
 	return parser.parse(str.trim())
