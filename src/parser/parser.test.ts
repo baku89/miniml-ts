371c98b129b48d5ab828b5ad32841c987fdc0842
Baku Hashimoto <mail@baku89.com>
2021-10-21 17:28:02 +0900

    Describe parser test

---
+++ src/parser/parser.test.ts
@@ -0,0 +1,17 @@
+import {parse} from '.'
+
+describe('parser', () => {
+	runTest('1', '1')
+	runTest('true', 'true')
+	runTest('false', 'false')
+	runTest('1 * 2 + 3', '((1 * 2) + 3)')
+	runTest('1 < 3', '(1 < 3)')
+	runTest('1 + 2 < 3', '((1 + 2) < 3)')
+
+	function runTest(input: string, expected: string) {
+		test(`${input} to be parsed as ${expected}`, () => {
+			const exp = parse(input)
+			expect(exp.print()).toBe(expected)
+		})
+	}
+})
