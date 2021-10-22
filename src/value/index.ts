b77f02a6be672243047aac90460089db29512273
Baku Hashimoto <mail@baku89.com>
2021-10-22 11:18:47 +0900

    Implement print method for Value

--- src/value/index.ts
+++ src/value/index.ts
@@ -1,13 +1,25 @@
 export type Value = Int | Bool
 
-export class Int {
+interface IValue {
+	print(): string
+}
+
+export class Int implements IValue {
 	public type: 'int' = 'int'
 
 	public constructor(public value: number) {}
+
+	public print(): string {
+		return this.value.toString()
+	}
 }
 
-export class Bool {
+export class Bool implements IValue {
 	public type: 'bool' = 'bool'
 
 	public constructor(public value: boolean) {}
+
+	public print(): string {
+		return this.value ? 'true' : 'false'
+	}
 }
